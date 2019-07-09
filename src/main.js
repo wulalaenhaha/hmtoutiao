813127ac09572190b7c211d700663e3477032ced
gdd <1356871952@qq.com>
2019-07-09 16:31:25 +0800

    inital

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 职责1：导入项目需要的资源（包，css）
+// 职责2：初始化跟实例 new Vue() 选项，
+// render是把某一个app组件渲染到指定的
+// 容器内(.$mount('#app'))内
 new Vue({
   render: h => h(App),
 }).$mount('#app')
